d00c52c4ac4b73fbf1703c543389b28e36815891
Renee Dewolf <santoshamom@gmail.com>
2018-01-17 20:46:05 -0500

    fix indentation

--- scraper.py
+++ scraper.py
@@ -1,17 +1,18 @@
 import scrapy
 
+
 class BrickSetSpider(scrapy.Spider):
-    name = "brickset_spider"
+    name = 'brick_spider'
     start_urls = ['http://brickset.com/sets/year-2016']
 
-def parse(self, response):
-    SET_SELECTOR = '.set'
-    for brickset in response.css(SET_SELECTOR):
-         
-         NAME_SELECTOR = 'h1 a ::text'
-         PIECES_SELECTOR = './/dl[dt/text() = "Pieces"]/dd/a/text()'
-         MINIFIGS_SELECTOR = './/dl[dt/text() = "Minifigs"]/dd[2]/a/text()'
-         IMAGE_SELECTOR = 'img ::attr(src)'
+    def parse(self, response):
+        SET_SELECTOR = '.set'
+        for brickset in response.css(SET_SELECTOR):
+
+            NAME_SELECTOR = 'h1 a ::text'
+            PIECES_SELECTOR = './/dl[dt/text() = "Pieces"]/dd/a/text()'
+            MINIFIGS_SELECTOR = './/dl[dt/text() = "Minifigs"]/dd[2]/a/text()'
+            IMAGE_SELECTOR = 'img ::attr(src)'
             yield {
                 'name': brickset.css(NAME_SELECTOR).extract_first(),
                 'pieces': brickset.xpath(PIECES_SELECTOR).extract_first(),
@@ -19,10 +20,10 @@ def parse(self, response):
                 'image': brickset.css(IMAGE_SELECTOR).extract_first(),
             }
 
-    NEXT_PAGE_SELECTOR = '.next a ::attr(href)'
-    next_page = response.css(NEXT_PAGE_SELECTOR).extract_first()
-    if next_page:
-        yield scrapy.Request(
-            response.urljoin(next_page),
-            callback=self.parse
+        NEXT_PAGE_SELECTOR = '.next a ::attr(href)'
+        next_page = response.css(NEXT_PAGE_SELECTOR).extract_first()
+        if next_page:
+            yield scrapy.Request(
+                response.urljoin(next_page),
+                callback=self.parse
             )
